b6ca0b4ba52cf33fecb00b405b5227cb7fb0e990
Rashmi24041997 <61780301+Rashmi24041997@users.noreply.github.com>
2022-09-23 16:26:48 +0530

    Intial Commit

---
+++ ASP.NET FullStack Application/WebApplication1/Data/NotesDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotesAPI.Models.Entities;
+
+namespace NotesAPI.Data
+{
+    //DbContext will help to communicate with the database
+    public class NotesDBContext : DbContext
+    {
+        public NotesDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //This will act as Table in database
+       public DbSet<Note> Notes  { get; set; }
+    }
+}
